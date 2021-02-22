5ac974284dd656354c9011792e5d786babd39471
decentration <ajramseys@gmail.com>
2021-02-22 09:38:26 +0000

    Decentrify thanks to parity

--- custom/hub.js
+++ custom/hub.js
@@ -2,9 +2,9 @@ var substrate_developer_hub_navbar = `
 <div class="substrate-dev-hub-nav">
     <a href="https://substrate.dev">
         <img class="logo" 
-            src="https://substrate.dev/img/Substrate-logo.svg"
-            alt="Substrate Developer Hub">
-        <h2 class="headerTitleWithLogo">Substrate Developer Hub</h2>
+            src= "/media/decentration-logo-1.png"
+            alt="Decentration Workshop">
+        <h2 class="headerTitleWithLogo">Decentration Workshop</h2>
     </a>
 </div>
 `;
